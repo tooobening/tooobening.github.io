9e72861e7c4657ed10683ce43ad1fe82b56285f3
tooobening <48101049+tooobening@users.noreply.github.com>
2020-12-11 12:31:54 -0600

    add 8 portfolio prokects

--- js/main.js
+++ js/main.js
@@ -12,3 +12,49 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open'); //remove certain class
     })
 })
+
+
+
+//popup
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+//set content in popup
+const popupText =document.querySelectorAll('.text');
+const popupT =document.querySelectorAll('.title');
+
+    console.log(popupText[1].innerText)
